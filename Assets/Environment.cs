be1203975c24033abdd56244ca5a1f87a715802e
jluta <jluta@datto.com>
2019-10-08 15:46:14 -0400

    all the stuffs

---
+++ Assets/Environment.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    const float cameraSize = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var width = (Camera.main.orthographicSize / 2880) * 1440;
+        Camera.main.orthographicSize = (width / Screen.width) * Screen.height;
+
+        SetupWalls();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetupWalls()
+    {
+        float actualCameraSize = Camera.main.orthographicSize;
+
+        GameObject walls = new GameObject();
+        walls.name = "Walls";
+
+        PhysicsMaterial2D mat = new PhysicsMaterial2D
+        {
+            friction = 0.0f,
+            bounciness = 0.5f,
+        };
+
+        // ceiling
+        BoxCollider2D collider0 = walls.AddComponent<BoxCollider2D>();
+        collider0.size = new Vector2(cameraSize, 0.25f);
+        collider0.offset = new Vector2(0, actualCameraSize + (0.25f / 2));
+        collider0.sharedMaterial = mat;
+
+        // floor
+        BoxCollider2D collider1 = walls.AddComponent<BoxCollider2D>();
+        collider1.size = new Vector2(cameraSize, 0.25f);
+        collider1.offset = new Vector2(0, -actualCameraSize - (0.25f / 2));
+
+        // left
+        BoxCollider2D collider2 = walls.AddComponent<BoxCollider2D>();
+        collider2.size = new Vector2(0.25f, actualCameraSize * 2);
+        collider2.offset = new Vector2(-cameraSize / 2 - (0.25f / 2), 0);
+        collider2.sharedMaterial = mat;
+
+        // right
+        BoxCollider2D collider3 = walls.AddComponent<BoxCollider2D>();
+        collider3.size = new Vector2(0.25f, actualCameraSize * 2);
+        collider3.offset = new Vector2(cameraSize / 2 + (0.25f / 2), 0);
+        collider3.sharedMaterial = mat;
+    }
+}
---
+++ Assets/Force.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Force : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool forced;
+
+    public bool time;
+    public float force = 1.0f;
+
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        //if (time)
+        //{
+        //    rb.AddForce(new Vector2(0.05f, 1) * 2000.0f * force * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        //}
+        //else
+        //{
+        //    rb.AddForce(new Vector2(0.05f, 1) * 40.0f * force, ForceMode2D.Impulse);
+        //}
+
+        //startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (!forced)
+        //{
+        //    rb.AddForce(transform.up * 10.0f, ForceMode2D.Impulse);
+        //    forced = true;
+        //}
+        //rb.MovePosition(rb.position + new Vector2(0, 1) * Time.fixedDeltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = transform.position;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 dir = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition) - startPos;
+            //rb.AddForce(dir.normalized * dir.magnitude * 2, ForceMode2D.Impulse);
+            rb.AddForce(dir * 2, ForceMode2D.Impulse);
+        }
+    }
+}
